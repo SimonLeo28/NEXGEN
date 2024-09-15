8eb0fc2a1f3c38f9d2de3f8f54c29c313e2d95a3
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2024-09-15 23:01:10 +0530

    Editted the course details

--- app/course/page.js
+++ app/course/page.js
@@ -2,8 +2,69 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      This is page for single course detail.
+    <>
+    <div className='flex-col gap-5'>
+      <div className='bg-[#2596BE]'>
+        <div className='p-10'>
+        <h1 className='mt-10 ml-10 text-white text-4xl font-bold'>100 Days of Code: The Complete Python<br/> Pro Bootcamp.</h1>
+        <p className='ml-10 text-white-600'>Master Python by building 100 projects in 100 days. Learn data science,<br/> automation, build websites, games and apps!</p>
+        </div>
+      </div>
+      <div className=' w-fit p-5 ml-10 mt-10'>
+        <h1 className='text-2xl font-bold text-black'>What you'll learn</h1>
+        <div className='flex gap-3'>
+          <div>
+          <li className='text-black mt-3 ml-7'>You will master the Python programming <br/> 
+          language by building 100 unique projects <br/>
+          over 100 days.          
+          </li>
+          <li className='text-black ml-7'>You will be able to program in Python  <br/> 
+          prefessionally.          
+          </li>
+          <li className='text-black ml-7'>Create a portfolio of 100 Python projects to <br/> 
+          apply for developer jobs.
+          </li>
+          </div>
+          <div>
+          <li className='text-black mt-3 ml-7'>You will master the Python programming <br/> 
+          language by building 100 unique projects <br/>
+          over 100 days.          
+          </li>
+          <li className='text-black ml-7'>You will be able to program in Python  <br/> 
+          prefessionally.          
+          </li>
+          <li className='text-black ml-7'>Create a portfolio of 100 Python projects to <br/> 
+          apply for developer jobs.
+          </li>
+          </div>
+          </div>
+      </div>
+      <div className=' w-[50%] mt-10 ml-10 h-[50%]'>
+        <h1 className='p-3 text-black text-2xl font-bold'>Course Content</h1>
+        <h1 className='text-black p-1 ml-10'>Course videos...</h1>
+      </div>
+      <div className='ml-10 mt-10 w-[70%] '>
+        <h1 className='p-3 text-2xl font-bold text-black'>Requirements...</h1>
+        <div className='p-5'>
+        <li className='p-1 text-black'>No programming experience needed - I'll teach you everything you need to know</li>
+        <li className='p-1 text-black'>A Mac or PC computer with access to the internet</li>
+        <li className='p-1 text-black'>No paid software required - I'll teach you how to use PyCharm, Jupyter Notebooks and Google Colab</li>
+        <li className='p-1 text-black'>I'll walk you through, step-by-step how to get all the software installed and set up</li>
+        </div>
+      </div>
+      <div className='  mt-10 ml-10 w-[80%]'>
+        <h1 className='text-black font-bold text-2xl p-3'>Description</h1>
+        <p className='text-black'>Here will be the course description sliced accordingly and have a read more and then will display the text.</p>
+      </div>
+      <div className='p-5'>
+        <h1 className='text-black font-bold text-2xl p-3 ml-3'>Who is this course for?</h1>
+        <li className='ml-10 p-1 text-black'>If you want to learn to code from scratch through building fun and useful projects, then take this course.</li>
+        <li className='ml-10 p-1 text-black'>If you want to start your own startup by building your own websites and web apps.</li>
+        <li className='ml-10 p-1 text-black'>If you are a complete beginner then this course will be everything you need to become a Python <br/>Professional.</li>
+        <li className='ml-10 p-1 text-black'>If you are a seasoned programmer wanting to switch to Python then this is the quickest way. Learn through <br/>coding projects.</li>
+        <li className='ml-10 p-1 text-black'>If you are an intermediate Python programmer then you know 100 days of code challenges will help you  <br/>Level up.</li>
+      </div>
     </div>
+    </>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,17 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+       <nav className="bg-[#2596BE] p-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        <h1 className="text-white text-2xl font-bold">NEXGEN</h1>
+        <div className="space-x-4">
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Courses</a>
+            <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Contact</a>
+        </div>
+      </div>
+    </nav>
         {children}
       </body>
     </html>
