9814fe8db477ae871658f8643306e3692aa4f6e8
SimonLeo28 <simonleoalexanderpcmct2023@gmail.com>
2024-10-02 18:48:34 +0530

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -253,46 +253,9 @@ export default function Home() {
    <div className="p-0 flex justify-center items-center">
     <h1 className="text-5xl font-extrabold text-black">Why Choose Us? </h1>
    </div>
-   {/*<div className="flex flex-col gap-[10%] justify-center items-center h-[1000px] ml-20 mr-20"> */}
-    {/* <div className="grid grid-cols-2  grid-rows-1">
-      <h1 className="flex justify-start items-start text-4xl font-extrabold text-gray-600">Comprehensive Learning</h1>
-      <p className="text-black font-medium ml-[50px]">Our diverse range of courses covers everything from essential technical skills <br/>
-      to in-demand soft skills. Access a wide range of courses designed to meet <br/>
-      industry demands and enhance your skills.
-      </p>
-    </div>
-    <div className="grid grid-cols-2 grid-rows-1">
-      <h1 className="flex justify-start items-start text-4xl font-extrabold text-gray-600">Expert-Led Instruction</h1>
-      <p className="text-black font-medium ml-[50px]">Learn from industry leaders and seasoned professionals who bring real-world  <br/>
-      experience and insights into every lesson. Our instructors are committed to  <br/>
-      providing practical knowledge that you can apply directly to your career.
-      </p>
-    </div>
-    <div className="grid grid-cols-2 grid-rows-1">
-      <h1 className="flex justify-start items-start text-4xl font-extrabold text-gray-600">Flexible Learning Experience</h1>
-      <p className="text-black font-medium ml-[50px]">Enjoy the freedom to learn at your own pace with our flexible course formats.   <br/>
-      Access high-quality video lectures, interactive quizzes, and hands-on projects  <br/>
-      whenever and wherever you prefer.
-      </p>
-    </div>
-    <div className="grid grid-cols-2 grid-rows-1">
-      <h1 className="flex justify-start items-start text-4xl font-extrabold text-gray-600">Real-Time Industry Insights</h1>
-      <p className="text-black font-medium ml-[50px]">Stay ahead of the curve with courses that reflect the latest trends and    <br/>
-      technologies. Our content is regularly updated to ensure you’re learning the   <br/>
-      most current and relevant skills.
-      </p>
-    </div>
-    <div className="grid grid-cols-2 grid-rows-1">
-      <h1 className="flex justify-start items-start text-4xl font-extrabold text-gray-600">Affordable and Accessible</h1>
-      <p className="text-black font-medium ml-[50px]">Get access to high-quality education without breaking the bank. Our courses   <br/>
-      are priced competitively, and we offer various discounts and promotions to   <br/>
-      make learning more accessible.
-      </p>
-    </div>  */}
     <div className="w-full">
       <Timeline data={data} />
     </div>
-   {/* </div> */}
   
    <footer className="bg-black text-[#2596BE] py-6 mt-10">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
